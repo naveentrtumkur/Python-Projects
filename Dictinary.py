5399973f9232e00e06367e487b37e52e97a188ae
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2017-11-04 00:24:32 -0400

    try,except case

--- Dictinary.py
+++ Dictinary.py
@@ -4,27 +4,30 @@ import re
 import urllib.request
 
 url = "http://dictionary.reference.com/browse/"
+try:
+    word = input("Enter your word:")
 
-word = input("Enter your word:")
+    url = url + word
 
-url = url + word
+    data = urllib.request.urlopen(url).read()
+    data1 = data.decode("utf-8")
 
-data = urllib.request.urlopen(url).read()
-data1 = data.decode("utf-8")
+    #print(data1)
 
-#print(data1)
+    m = re.search('meta name="description" content="', data1)
+    start = m.end()
+    end = start + 300
 
-m = re.search('meta name="description" content="', data1)
-start = m.end()
-end = start + 300
+    newString = data1[start:end]
 
-newString = data1[start:end]
+    #print(newString)
 
-#print(newString)
+    m = re.search("See more.", newString)
+    end = m.start() - 1
 
-m = re.search("See more.", newString)
-end = m.start() - 1
+    defn  = newString[0:end]
 
-defn  = newString[0:end]
+    print(defn)
 
-print(defn)
+except:
+    print("Your word was not found in dictionary")
